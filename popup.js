5ea493efdab3f472d4a4a0e3be3f7f545d18ea3a
SHARKYBOY1248 <ajitkadaveru@yahoo.com>
2021-01-14 15:55:40 -0500

    Update popup.js
    ok so it works for manually adding problems(buttons), stiill need it to get for adding through for loop of problems.

--- popup.js
+++ popup.js
@@ -1,60 +1,11 @@
 var problems = [];
 var solves = [];
-// document.addEventListener('DOMContentLoaded', function(){
-//     document.getElementById("Z0").addEventListener('click',onclick, false)
-//     function onclick(){
-//         var current = document.getElementById("Z0").innerText;
-//         if (isNaN(parseInt(current))){
-//             chrome.runtime.onMessage.addListener(
-//                 function(request, sender, sendResponse){
-//                     var solves = request.sol;
-//                     document.getElementById("Z0").innerText = solves[0];
-//                 }
+ // ok for some reason it doesn't receive future messages
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+   problems = request.pro;
+   solves = request.sol;
+})
 
-//             );
-            
-//         }
-//         else{
-//             chrome.runtime.onMessage.addListener(
-//                 function(request, sender, sendResponse){
-//                     var solves = request.sol;
-//                     document.getElementById("Z0").innerText = problems[0];
-//                 }
-
-//             );
-//         }
-//     }
-// })
-
-// document.addEventListener('DOMContentLoaded', function(){
-//     document.getElementById("Z1").addEventListener('click',onclick, false)
-//     function onclick(){
-//         var current = document.getElementById("Z1").innerText;
-//         if (isNaN(parseInt(current))){
-//             chrome.runtime.onMessage.addListener(
-//                 function(request, sender, sendResponse){
-//                     var solves = request.sol;
-//                     document.getElementById("Z1").innerText = solves[1];
-//                 }
-
-//             );
-            
-//         }
-//         else{
-//             chrome.runtime.onMessage.addListener(
-//                 function(request, sender, sendResponse){
-//                     var solves = request.sol;
-//                     document.getElementById("Z1").innerText = problems[1];
-//                 }
-
-//             );
-//         }
-//     }
-// })
- // WHY IS IT NOT RECEIVING MESSAGE
-
-
-// for(var j = 0;j<problems.length;j++){
     var button = document.createElement('button');
     button.type = 'button';
     button.innerText = 'wait darn';
@@ -64,43 +15,51 @@ var solves = [];
         function onclick(){
             var current = button.innerText;
             if (isNaN(parseInt(current))){
-                
-                // chrome.runtime.onMessage.addListener(
-                //     function(request, sender, sendResponse){
-                //         var solves = request.sol;
-                //         document.getElementById("Z0").innerText = solves[0];
-                //     }
-
-                // );        
-                chrome.runtime.onMessage.addListener(
-                    function(request, sender, sendResponse){
-                        solves = request.sol;
-                        problems = request.pro;
-                        button.innerText = solves[0];
-                    }
-                
-                );
-                
+                button.innerText = solves[0];               
             }
             else{
-                chrome.runtime.onMessage.addListener(
-                    function(request, sender, sendResponse){
-                        solves = request.sol;
-                        problems = request.pro;
-                        button.innerText = problems[0];
-                    }
-                
-                );
-                // chrome.runtime.onMessage.addListener(
-                //     function(request, sender, sendResponse){
-                //         var solves = request.sol;
-                //         document.getElementById("Z0").innerText = problems[0];
-                //     }
-
-                // );
+                button.innerText = problems[0];
                 
             }
         }
     }, false);
     document.body.appendChild(button);
+
+    var button1 = document.createElement('button');
+    button1.type = 'button';
+    button1.innerText = 'wait darn';
+    button1.className = 'btn-styled';
+    document.addEventListener('DOMContentLoaded', function(){
+        button1.addEventListener('click', onclick, false)
+        function onclick(){
+            var current = button1.innerText;
+            if (isNaN(parseInt(current))){
+                button1.innerText = solves[1];               
+            }
+            else{
+                button1.innerText = problems[1];
+                
+            }
+        }
+    }, false);
+    document.body.appendChild(button1);
+
+    var button2 = document.createElement('button');
+    button2.type = 'button';
+    button2.innerText = 'wait darn';
+    button2.className = 'btn-styled';
+    document.addEventListener('DOMContentLoaded', function(){
+        button2.addEventListener('click', onclick, false)
+        function onclick(){
+            var current = button2.innerText;
+            if (isNaN(parseInt(current))){
+                button2.innerText = solves[2];               
+            }
+            else{
+                button2.innerText = problems[2];
+                
+            }
+        }
+    }, false);
+    document.body.appendChild(button2);
 // }
